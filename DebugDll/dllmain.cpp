cf8fcf162aea796fec7c0805111a2fecc41de667
Gareth Evans <gazz.sf@gmail.com>
2022-07-11 18:08:45 -0700

    Change debug dll to a MessageBox

--- DebugDll/dllmain.cpp
+++ DebugDll/dllmain.cpp
@@ -7,10 +7,17 @@ DllMain(
     LPVOID lpReserved
 )
 {
+    int msgboxID;
+
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        Sleep(10000);
+        msgboxID = MessageBox(
+            NULL,
+            (LPCWSTR)L"Hello from injected dll",
+            (LPCWSTR)L"Injection successful",
+            MB_ICONWARNING | MB_OK
+        );
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
